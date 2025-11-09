a418e261ea2fd38403d4c8de12973459a54122b6
MOHAMMAD AKASH <mohammadakashiam@gmail.com>
2025-11-09 08:37:13 +0600

    Update setname.js

--- Script/commands/setname.js
+++ Script/commands/setname.js
@@ -4,7 +4,7 @@ module.exports.config = {
 	hasPermssion: 0,
 	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
 	description: "Change the nickname in your group or the person you tag",
-	commandCategory: "Box Chat",
+	commandCategory: "Box",
 	usages: "[name]",
 	cooldowns: 3
 };
